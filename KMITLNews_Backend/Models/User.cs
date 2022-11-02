7bdec99d5c4a356045569a2c43d68c9d5239ba11
thepunnn <69188795+PunPunyawat@users.noreply.github.com>
2022-11-02 16:45:53 +0700

    Backend User API

--- KMITLNews_Backend/Models/User.cs
+++ KMITLNews_Backend/Models/User.cs
@@ -1,21 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace KMITLNews_Backend.Models {
-	public class User {
-		[Key]
-		public int UserID { get; set; }
-
-		public string Email { get; set; } = string.Empty;
-		public string PassHash { get; set; } = string.Empty;
-		public string MobileNo { get; set; } = string.Empty;
-
-		public string FName { get; set; } = string.Empty;
-		public string LName { get; set; } = string.Empty;
-		public string? ProfilePicUrl { get; set; }
-		public string DisplayName { get; set; } = string.Empty;
-
-		public int UserType { get; set; }
-		public int VerifiedStatus { get; set; }
-		public int ReportCount { get; set; }
-	}
+namespace UserAPI.Models
+{
+    // Real schema 
+    public class User
+    {
+        [Key]  // บอกถึงว่าเป็น primary key
+        public int user_id { get; set; } 
+        public string email { get; set; } = string.Empty;
+        public byte[] pass_hash { get; set; } = new byte[32];
+        public byte[] pass_salt { get; set; } = new byte[32];
+        public string mobile_no { get; set; } = string.Empty;
+        public string first_name { get; set; } = string.Empty;
+        public string last_name { get; set; } = string.Empty;
+        public string verificationToken { get; set; } = string.Empty;
+        public string profile_pic_url { get; set; } = string.Empty;
+        public string display_name { get; set; } = string.Empty;
+        public int user_type { get; set; }
+        public int verified { get; set; }
+        public int report_count { get; set; }
+        
+    }
 }
