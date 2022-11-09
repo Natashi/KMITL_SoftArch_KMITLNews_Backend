58a4a1884e00a6546b6d2c3828f99fb177793356
WEB RATTNA <nantawat.385@gmail.com>
2022-11-09 21:12:05 +0700

    post

---
+++ KMITLNews_Backend/Models/Post_Delete.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace KMITLNews_Backend.Models
+{
+    public class Post_Delete
+    {
+        [Required]
+        public int Post_id { get; set; }
+
+    }
+	
+}
