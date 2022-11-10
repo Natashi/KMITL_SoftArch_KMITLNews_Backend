ce5574b8644259c3fc04c6b578e2ecea7c04f1db
WEB RATTNA <nantawat.385@gmail.com>
2022-11-10 14:06:16 +0700

    Create Post_Create.cs

---
+++ KMITLNews_Backend/Models/Post_Create.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace KMITLNews_Backend.Models
+{
+    public class Post_Create
+    {
+        [Required]
+        public int user_id { get; set; }
+        
+        [Required(AllowEmptyStrings=false)]
+        public string post_text { get; set; } = string.Empty;
+        [Required]
+        public string attached_image_url { get; set; } = string.Empty;
+
+        public int shared_post_id { get; set; }
+ 
+    }
+	
+}
