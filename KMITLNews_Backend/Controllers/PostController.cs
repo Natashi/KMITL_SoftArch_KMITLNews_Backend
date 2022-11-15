43ab57837b5bb1c3c06e87a2b38bb2e05f3ab2d2
Natashi <32347635+Natashi@users.noreply.github.com>
2022-11-15 20:51:34 +0700

    Cleaning (incomplete)

--- KMITLNews_Backend/Controllers/PostController.cs
+++ KMITLNews_Backend/Controllers/PostController.cs
@@ -3,118 +3,108 @@ using Microsoft.AspNetCore.Mvc;
 using KMITLNews_Backend.Models;
 using System.Linq;
 
-
-namespace PostAPI.Controllers
-{
-    [Route("api/[controller]")] // ตรวจค่าว่าเอาคำที่อยู่หน้า Controller มาเป็นชื่อ Route
+namespace PostAPI.Controllers {
+	[Route("api/[controller]")] // ตรวจค่าว่าเอาคำที่อยู่หน้า Controller มาเป็นชื่อ Route
 	[ApiController]
+	public class PostController : ControllerBase {
+		private readonly DataContext _context;
 
-    public class PostController : ControllerBase{
-        private readonly DataContext _context;
-
-        
-        
-       public PostController(DataContext context)
-		{
+		public PostController(DataContext context) {
 			_context = context;
 		}
-    
-    public async Task<ActionResult<IEnumerable<Post>>> GetAllPost()
-		{
+
+		public async Task<ActionResult<IEnumerable<Post>>> GetAllPost() {
 			return await _context.Posts.ToListAsync();
 		}
 
-    [HttpPost("CreatePost")]
-    public async Task<ActionResult> PostCreate(Post_Create request){
-        
-        	var post = new Post
-			{
-				post_date = DateTime.Now,
-				post_text = request.post_text,
-				attached_image_url = request.attached_image_url,
-                verified = false,
-                report_count =0,
-		
+		[HttpPost("CreatePost")]
+		public async Task<ActionResult> PostCreate(Post_Create request) {
 
+			var post = new Post {
+				post_date = DateTime.Now,
+
+				post_text = request.PostText,
+				attached_image_url = request.AttachedImgUrl,
+
+				verified = false,
+				report_count = 0,
 			};
 
 			_context.Posts.Add(post);
-			await _context.SaveChangesAsync(); 
+			await _context.SaveChangesAsync();
 
-      var Post_U =await _context.Posts.FirstOrDefaultAsync(u=>u.post_text == request.post_text);
-      if (Post_U.post_id==null) return BadRequest("not found Post_id");
-      var P_User= new Posts_Users
-     {
+			var Post_U = await _context.Posts.FirstOrDefaultAsync(u => u.post_text == request.PostText);
+			if (Post_U.post_id == null) return BadRequest("not found Post_id");
+			var P_User = new Posts_Users {
+				user_id = request.UserID,
+				post_id = Post_U.post_id,
 
-         user_id = id,
-         post_id = Post_U.post_id,
-        
-      };
-      _context.Posts_Users.Add(P_User);
-			await _context.SaveChangesAsync(); 
-			
+			};
+			_context.Posts_Users.Add(P_User);
+			await _context.SaveChangesAsync();
+
 			return Ok("success");
-   
-
-    }
 
 
-      [HttpPut("UpdatePost/{id}")]
-       public async Task<ActionResult> Postupdate(int id,Post_Update request){
-        var Post_id =await _context.Posts.FirstOrDefaultAsync(u=>u.post_id == request.post_id);
-        var User_id =await _context.Users.FirstOrDefaultAsync(u=>u.user_id == id);
-        if(Post_id==null || User_id==null) return BadRequest("not found");
-        
-        Post_id.post_text = request.post_text;
-        Post_id.post_date = DateTime.Now;
+		}
+
+
+		[HttpPut("UpdatePost/{id}")]
+		public async Task<ActionResult> Postupdate(int id, Post_Update request) {
+			var Post_id = await _context.Posts.FirstOrDefaultAsync(u => u.post_id == request.post_id);
+			var User_id = await _context.Users.FirstOrDefaultAsync(u => u.user_id == id);
+			if (Post_id == null || User_id == null) return BadRequest("not found");
+
+			Post_id.post_text = request.post_text;
+			Post_id.post_date = DateTime.Now;
 
 			return Ok("Post Edit success");
-   
-
-    }
-
-    [HttpPut("DeletePost/{id}")]
-       public async Task<ActionResult> Postdelete(int id,Post_Delete request){
-        var Post_id_Post = await _context.Posts.FindAsync(request.post_id);
-        var Post_id_PU = await _context.Posts_Users.FindAsync(request.post_id);
-        var Post_id_TP = await _context.Tags_Posts.FindAsync(request.post_id);
-        var Post_id_Share = await _context.Users_SharedPosts.FindAsync(request.post_id);
-        if(Post_id_Post==null && Post_id_PU.post_id == id &&Post_id_PU.post_id != null ) return BadRequest("not found");
-    
-        _context.Posts.Remove(Post_id_Post);
-        _context.Posts_Users.Remove(Post_id_PU);
-        _context.Tags_Posts.Remove(Post_id_TP);
-        _context.Users_SharedPosts.Remove(Post_id_Share);
-
-        await _context.SaveChangesAsync();
-        return Ok("Post Delete success");
-    }
 
 
-     [HttpGet("GetPostReportCount")]
-       public async Task<ActionResult> GetPostReportCount(){
-        return Ok(await _context.Posts.Where(u=>u.report_count > 0).ToListAsync());
-    }
+		}
 
-    [HttpGet("GetPostTags/{tag}")]
-       public async Task<ActionResult> GetPostTags(string tag){
-     //   
-      return Ok(await _context.Tags_Follows.Where(u=>u.tag_name == tag).ToListAsync());
+		[HttpPut("DeletePost/{id}")]
+		public async Task<ActionResult> Postdelete(int id, Post_Delete request) {
+			var Post_id_Post = await _context.Posts.FindAsync(request.post_id);
+			var Post_id_PU = await _context.Posts_Users.FindAsync(request.post_id);
+			var Post_id_TP = await _context.Tags_Posts.FindAsync(request.post_id);
+			var Post_id_Share = await _context.Users_SharedPosts.FindAsync(request.post_id);
+			if (Post_id_Post == null && Post_id_PU.post_id == id && Post_id_PU.post_id != null) return BadRequest("not found");
 
-    }
+			_context.Posts.Remove(Post_id_Post);
+			_context.Posts_Users.Remove(Post_id_PU);
+			_context.Tags_Posts.Remove(Post_id_TP);
+			_context.Users_SharedPosts.Remove(Post_id_Share);
 
-    [HttpGet("GetAllPost")]
-       public async Task<ActionResult> GetAllPostc(){
-     //   
-      return Ok(await _context.Posts.Where(u=>u.verified== true).ToListAsync());
+			await _context.SaveChangesAsync();
+			return Ok("Post Delete success");
+		}
 
-    }
 
-        [HttpGet("GetAllPostbyUser/{id}")]
-       public async Task<ActionResult> GetAllPostbyUser(int id){
-     //   
-      return Ok(await _context.Posts_Users.Where(u=>u.user_id==id).ToListAsync());
+		[HttpGet("GetPostReportCount")]
+		public async Task<ActionResult> GetPostReportCount() {
+			return Ok(await _context.Posts.Where(u => u.report_count > 0).ToListAsync());
+		}
 
-    }
-}
+		[HttpGet("GetPostTags/{tag}")]
+		public async Task<ActionResult> GetPostTags(string tag) {
+			//   
+			return Ok(await _context.Tags_Follows.Where(u => u.tag_name == tag).ToListAsync());
+
+		}
+
+		[HttpGet("GetAllPost")]
+		public async Task<ActionResult> GetAllPostc() {
+			//   
+			return Ok(await _context.Posts.Where(u => u.verified == true).ToListAsync());
+
+		}
+
+		[HttpGet("GetAllPostbyUser/{id}")]
+		public async Task<ActionResult> GetAllPostbyUser(int id) {
+			//   
+			return Ok(await _context.Posts_Users.Where(u => u.user_id == id).ToListAsync());
+
+		}
+	}
 }
--- KMITLNews_Backend/Models/Post_Create.cs
+++ KMITLNews_Backend/Models/Post_Create.cs
@@ -5,11 +5,13 @@ namespace KMITLNews_Backend.Models
     public class Post_Create
     {
         [Required]
-        public int user_id { get; set; }
-        [Required(AllowEmptyStrings=false)]
-        public string post_text { get; set; } = string.Empty;
+        public int UserID { get; set; }
+
         [Required]
-        public string attached_image_url { get; set; } = string.Empty;
+        public string PostText { get; set; } = string.Empty;
+
+        [Required]
+        public string AttachedImgUrl { get; set; } = string.Empty;
     }
 	
 }
