4fc48349c9039afcc865454d1b73bf37bb4cad69
Natashi <32347635+Natashi@users.noreply.github.com>
2022-11-17 20:23:20 +0700

    Fixed copy-paste mistakes

--- KMITLNews_Backend/Models/Tags_Posts.cs
+++ KMITLNews_Backend/Models/Tags_Posts.cs
@@ -3,7 +3,7 @@
 namespace KMITLNews_Backend.Models {
 	[Keyless]
 	public class Tags_Posts {
+		public string tag_name { get; set; } = string.Empty;
 		public int post_id { get; set; }
-		public int shared_post_id { get; set; }
 	}
 }
